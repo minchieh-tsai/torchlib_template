9c8c2d0c65651860d40f57059e87675ccb75ad49
minchieh-tsai <minchieh1997@gmail.com>
2024-05-26 18:39:53 +0800

    start

--- app/main.cpp
+++ app/main.cpp
@@ -3,36 +3,61 @@
 #include <lib1/lib1-pub-header.h>
 #include <torch/torch.h>
 #include <torch/script.h>
+#include <opencv2/opencv.hpp>
 
-// test tempalate
-// int main(int argNum, char **argValue) {
-//   std::cout << "1 + 2 = " << lib1::sum(1, 2) << '\n';
-//   return 0;
-// }
+#include <filesystem>  
+namespace fs = std::filesystem;
 
-// test libtorch
-// int main()
-// {
-//     torch::DeviceType device_type = torch::kCPU;
-//     if (torch::cuda::is_available()) {
-//         std::cout << "CUDA available! Predicting on GPU." << std::endl;
-//         device_type = torch::kCUDA;
-//     }
-//     else {
-//         std::cout << "Predicting on CPU." << std::endl;
-//         device_type = torch::kCPU;
-//     }
-//     torch::Device device(device_type);
-//     torch::Tensor tensor = torch::eye(3);
-//     tensor = tensor.to(device_type);
-//     // std:cout<<tensor<<std::endl;
-//     std::cout<<"hello torch"<<std::endl;
-//     std::cout<<tensor<<std::endl;
-//     return 0;
-// }
+
+torch::Tensor cvMatToTensorGray(const cv::Mat& image) {
+
+    cv::Mat imageFloat;
+    image.convertTo(imageFloat, CV_32F, 1.0 / 255.0);
+    torch::Tensor tensorImage = torch::from_blob(imageFloat.data, {1, 1, imageFloat.rows, imageFloat.cols}, torch::kFloat);
+
+    return tensorImage.clone(); 
+}
+
+
+cv::Mat tensorToCvMatGray(const torch::Tensor& tensorImage) {
+
+    torch::Tensor tensor = tensorImage.detach().clone();
+    tensor = tensor.squeeze(0);
+
+    tensor = tensor.mul(255).clamp(0, 255).to(torch::kU8);
+    int height = tensor.size(1);
+    int width = tensor.size(2);
+
+    cv::Mat image(cv::Size(width, height), CV_8UC1, tensor.data_ptr());
+
+    return image.clone(); 
+}
 
 
 int main(int argc, const char* argv[]) {
+
+    std::vector<cv::Mat> images;
+    std::string folderPath = "external_libs/testset"; 
+    
+
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".png") {
+            cv::Mat image = cv::imread(entry.path().string(), cv::IMREAD_COLOR);
+
+            if (!image.empty()) {
+                images.push_back(image);
+                std::cout << "Loaded image: " << entry.path() << std::endl;
+                std::cout << "Loaded image: " << image.size() << std::endl;
+            } else {
+                std::cerr << "Failed to load image: " << entry.path() << std::endl;
+            }
+        }
+    }
+
+    std::cout << "Total loaded images: " << images.size() << std::endl;
+
+
+
   if (argc != 2) {
     std::cerr << "usage: example-app <path-to-exported-script-module>\n";
     return -1;
@@ -53,13 +78,17 @@ int main(int argc, const char* argv[]) {
   }
   std::cout << "Model "<< argv[1]<<" loaded fine\n";
 
-  // Create a vector of inputs.
-  std::vector<torch::jit::IValue> inputs;
-  inputs.push_back(torch::ones({1, 3, 1024, 1024}));
 
-  // Execute the model and turn its output into a tensor.
-  at::Tensor output = module.forward(inputs).toTensor();
-  torch::Tensor slice = output[0][0][0].slice(0, 0, 100);
-  std::cout << slice << "\n";
+  for (auto& img : images) {
+        if (!img.empty()) {
+            torch::Tensor tensor = cvMatToTensorGray(img);
+            torch::Tensor output = module.forward({tensor}).toTensor();
+            std::cout << output.sizes() << std::endl;
+
+            cv::Mat mat_output  = tensorToCvMatGray(output);
+            bool isSaved = cv::imwrite("external_libs/output_dir/test.jpg", mat_output);
+        }
+    }
 
 }
+
